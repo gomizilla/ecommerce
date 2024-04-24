53d3711d41b0faa3baf5ade4d8af2138fbc34882
Dean <gomidean@gmail.com>
2024-04-24 23:16:06 +0900

    Add product page layout

--- app/add-product/page.tsx
+++ app/add-product/page.tsx
@@ -1,7 +1,42 @@
+export const metadata = {
+  title: "Add Product - Waaamazon",
+};
+
 export default function AddProductPage() {
   return (
     <div>
       <h1 className="mb-3 text-lg font-bold">Add Product</h1>
+      <form>
+        <input
+          required
+          name="name"
+          placeholder="Name"
+          className="input input-bordered mb-3 w-full"
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="Description"
+          className="textarea textarea-bordered mb-3 w-full"
+        />
+        <input
+          required
+          name="imageUrl"
+          placeholder="Image URL"
+          type="url"
+          className="input input-bordered mb-3 w-full"
+        />
+        <input
+          required
+          name="price"
+          placeholder="Price"
+          type="number"
+          className="input input-bordered mb-3 w-full"
+        />
+        <button className="btn btn-primary btn-block" type="submit">
+          Add Product
+        </button>
+      </form>
     </div>
   );
 }
