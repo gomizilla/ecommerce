aa0c2b248fd8d5ae2f04c0276abe6f431e0bacad
Dean <gomidean@gmail.com>
2024-04-29 23:55:39 +0900

    add: basic product layout, product card new depending on date uploaded

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -8,6 +8,9 @@ interface ProudctCardProps {
 }
 
 export default function ProductCard({ product }: ProudctCardProps) {
+  const isNew =
+    Date.now() - new Date(product.createdAt).getTime() <
+    1000 * 60 * 60 * 24 * 7;
   return (
     <Link
       href={"/products/" + product.id}
@@ -19,10 +22,12 @@ export default function ProductCard({ product }: ProudctCardProps) {
           alt={product.name}
           width={800}
           height={400}
+          className="h-48 object-cover"
         />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{product.name}</h2>
+        {isNew && <div className="badge badge-secondary">NEW</div>}
         <p>{product.description}</p>
         <PriceTag price={product.price} />
       </div>
