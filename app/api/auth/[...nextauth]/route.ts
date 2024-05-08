be09930efa8813f81aeddc51334fde4a0b640b8d
Dean <gomidean@gmail.com>
2024-05-08 17:53:49 +0900

    add: setting up for userauth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,21 @@
+import { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/app/lib/db/prisma";
+import { Adapter } from "next-auth/adapters";
+import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth/next";
+import { env } from "@/app/lib/env";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
