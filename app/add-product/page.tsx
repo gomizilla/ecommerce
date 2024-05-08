aceec407fa0dbbab9dd09fece723f29444dc35c5
Dean <gomidean@gmail.com>
2024-05-08 19:54:59 +0900

    add: some security for add product page if not logged in

--- app/add-product/page.tsx
+++ app/add-product/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 import { prisma } from "../lib/db/prisma";
 import FormSubmitButton from "@/components/FormSubmitButton";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export const metadata = {
   title: "Add Product - Waaamazon",
@@ -8,6 +10,12 @@ export const metadata = {
 
 async function addProduct(formData: FormData) {
   "use server";
+
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/add-product");
+  }
 
   const name = formData.get("name")?.toString();
   const description = formData.get("description")?.toString();
@@ -27,7 +35,13 @@ async function addProduct(formData: FormData) {
   redirect("/");
 }
 
-export default function AddProductPage() {
+export default async function AddProductPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/add-product");
+  }
+
   return (
     <div>
       <h1 className="mb-3 text-lg font-bold">Add Product</h1>
