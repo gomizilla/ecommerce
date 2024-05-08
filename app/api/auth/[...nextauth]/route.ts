672371eaa56bbf93871fc19943db6e3760e7637e
Dean <gomidean@gmail.com>
2024-05-08 20:59:24 +0900

    feat: anon cart merged when user logs in

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,6 +5,7 @@ import { Adapter } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth/next";
 import { env } from "@/app/lib/env";
+import { mergeAnonCartIntoUserCart } from "@/app/lib/db/cart";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
@@ -18,6 +19,11 @@ export const authOptions: NextAuthOptions = {
     session({ session, user }) {
       session.user.id = user.id;
       return session;
+    },
+  },
+  events: {
+    async signIn({ user }) {
+      await mergeAnonCartIntoUserCart(user.id);
     },
   },
 };
