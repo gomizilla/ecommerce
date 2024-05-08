7de959efce830d2b9a3847360e558a97c87a2c52
Dean <gomidean@gmail.com>
2024-05-08 22:53:58 +0900

    fix: moved authoptions to options file for vercel deployment

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,30 @@
+import { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/app/lib/db/prisma";
+import { Adapter } from "next-auth/adapters";
+import GoogleProvider from "next-auth/providers/google";
+import { env } from "@/app/lib/env";
+import { mergeAnonCartIntoUserCart } from "@/app/lib/db/cart";
+
+const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
+  events: {
+    async signIn({ user }) {
+      await mergeAnonCartIntoUserCart(user.id);
+    },
+  },
+};
+
+export default authOptions;
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,33 +1,5 @@
-import { NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/app/lib/db/prisma";
-import { Adapter } from "next-auth/adapters";
-import GoogleProvider from "next-auth/providers/google";
-import NextAuth from "next-auth/next";
-import { env } from "@/app/lib/env";
-import { mergeAnonCartIntoUserCart } from "@/app/lib/db/cart";
-
-const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma) as Adapter,
-  providers: [
-    GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  callbacks: {
-    session({ session, user }) {
-      session.user.id = user.id;
-      return session;
-    },
-  },
-  events: {
-    async signIn({ user }) {
-      await mergeAnonCartIntoUserCart(user.id);
-    },
-  },
-};
+import NextAuth from "next-auth";
+import authOptions from "./options";
 
 const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST, authOptions };
+export { handler as GET, handler as POST };
